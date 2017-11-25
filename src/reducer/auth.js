f632c225731c07c694110728853edc965729c755
Eddie <Eddie@Edwins-MacBook-Pro.local>
2017-11-24 16:22:48 -0800

    Added react template

---
+++ src/reducer/auth.js
@@ -0,0 +1,12 @@
+module.exports = (state=null, action) => {
+  let {type, payload} = action;
+
+  switch(type) {
+    case 'TOKEN_SET':
+      return payload;
+    case 'TOKEN_DELETE':
+      return null;
+    default:
+      return state;
+  }
+};
