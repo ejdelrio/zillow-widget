d293aad5bf098538f1a12a9919703461978d2748
Eddie <Eddie@Edwins-MacBook-Pro.local>
2017-11-24 17:50:32 -0800

    Began building listing widget

---
+++ src/component/listing/index.js
@@ -0,0 +1,19 @@
+import './_listing.scss';
+
+import React from 'react';
+
+class Listing extends React.Component {
+  constructor (props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <section id={'listing'}>
+
+      </section>
+    )
+  }
+}
+
+export default Listing;
