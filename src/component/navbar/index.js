f79b221a6e7a257ace809e0f644ce0ffa33f60e9
Eddie <Eddie@Edwins-MacBook-Pro.local>
2017-11-24 17:32:44 -0800

    Creatd logic to change class of selected button. Class change triggers color change of button

--- src/component/navbar/index.js
+++ src/component/navbar/index.js
@@ -6,8 +6,23 @@ class NavBar extends React.Component {
     super(props);
 
     this.state = {
-
+      priceClass: 'selected',
+      bedsClass: '',
+      sizeClass: ''
     };
+
+    this.buttonClick = this.buttonClick.bind(this);
+  }
+
+  buttonClick(buttonName) {
+    let newState = {...this.state};
+
+    for (let item in newState) {
+      if(item === buttonName) newState[item] = 'selected';
+      else newState[item] = '';
+    }
+    this.setState(newState);
+
   }
 
   render () {
@@ -15,14 +30,20 @@ class NavBar extends React.Component {
       <section id={'navbar'}>
         <h1>Awesome Listing Widget</h1>
         <ul>
-          <li>
-            <button>Price</button>
+          <li className={this.state.priceClass}>
+            <button onClick={() => this.buttonClick('priceClass')}>
+              Price
+            </button>
           </li>
-          <li>
-            <button>Beds</button>
+          <li className={this.state.bedsClass}>
+            <button onClick={() => this.buttonClick('bedsClass')}>
+              Beds
+            </button>
           </li>
-          <li>
-            <button>Sq. ft.</button>
+          <li className={this.state.sizeClass}>
+            <button onClick={() => this.buttonClick('sizeClass')}>
+              Sq. ft.
+            </button>
           </li>
         </ul>
       </section>
